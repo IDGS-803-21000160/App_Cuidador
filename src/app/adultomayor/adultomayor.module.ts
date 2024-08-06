1da49f8e46fa19c69ee3be23a2ac68f1d61f64f4
Ivan Blancarte <iblancarte583@gmail.com>
2024-08-06 15:13:39 -0600

    feat: estructura y modulo de Administrador funcional 70%

---
+++ src/app/adultomayor/adultomayor.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdultomayorRoutingModule } from './adultomayor-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AdultomayorRoutingModule
+  ]
+})
+export class AdultomayorModule { }
